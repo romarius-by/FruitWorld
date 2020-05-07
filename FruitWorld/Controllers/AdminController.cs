9b7256afd3a7c18ced81a38c7397148836ad03b3
valadyan <valadyanich@mail.ru>
2020-05-07 16:02:21 +0300

    Created admin panel(without CRUD)

---
+++ FruitWorld/Controllers/AdminController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using FruitWorld.Models;
+
+namespace FruitWorld.Controllers
+{
+    public class AdminController : Controller
+    {
+        IProductRepository _rep;
+        public AdminController(IProductRepository rep)
+        {
+            _rep = rep;
+        }
+        public IActionResult Index()
+        {
+            return View(_rep.Products);
+        }
+    }
+}
